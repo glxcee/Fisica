c6c4c527bb0610047bd67a2471ee2e3daf5079b4
nicoixx <niccolo.baroni@studio.unibo.it>
2025-05-19 12:16:04 +0200

    ho messo clang-format e doctest. ho fatto finta di iniziare a scrivere la teoria

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* x == densità di prede
+y == densità predatori
+A == tasso di crescita prede
+B == tasso di mortalità prede --> dipende da #prede e #predatori
+C == tasso di crescita predatori
+D == tasso di mortalità predatori --> dipende da #prede e #predatori
+
+
+
+*/
