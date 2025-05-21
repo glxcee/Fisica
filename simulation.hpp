2e2dfe713c9976314a3ad7f2715be3721247f68c
nicoixx <niccolo.baroni@studio.unibo.it>
2025-05-21 17:13:43 +0200

    ho impostato le equazioni discretizzate e il calcolo di H[i] con gli int, non so se è giusto

--- simulation.hpp
+++ simulation.hpp
@@ -2,43 +2,41 @@
 #include <vector>
 
 namespace pr {
-    
-    struct Result {
-        double X;
-        double Y;
-        double H;
-    };
 
-    struct Point {
-        double x;
-        double y;
-    };
+struct Result {
+  double X;
+  double Y;
+  double H;
+};
 
-    class Simulation {
-        
-        std::vector<Point> points_;
+/*struct Point {
+  double x;
+  double y;
+};*/
 
-        double A;
-        double B;
-        double C;
-        double D;
-        double n;
+class Simulation {
+  // std::vector<Point> points_;
 
-        public:
+  double A, B, C, D;
+  double n;
 
-        int pr::Simulation::evolve(
-            
-            double x_rel {x_rel = x*C/D;}
-            double y_rel {y_rel =};
-
-
-        );
-
-
+ public:
+  /*int pr::Simulation::*/ void evolve(int x, int y);  //non so se basta il void o se serve pr::Simulation:
+  double x{};
+  double y{};
+  for (int i = 1; i < n; ++i) {
+    x[i] = x[i - 1] + A * (1 - y[i - 1]) * x[i - 1] * t;
+    y[i] = y[i - 1] + D * (x[i - 1] - 1) * y[i - 1] * t;
+  }
+  if (i == n) {
+    for (int i = 0; i <= n; ++i) {
+      
+      double H = - D * std::log(x) + C * x + B * y - A * std::log(y); // calcolo H_i, credo
     }
+  }
+
+  ;
+}; 
 
 
-
-
-;}
-
+} // namespace pr
